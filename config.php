28df2b945da7363c5833acc544794725e61b6c36
Scarnel <rabbicitrix@live.com>
2014-07-30 16:08:09 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,1 @@
-<? session_start(); $conn = mysql_connect("localhost","pkmnazur_Citrix","Suicune75"); $db = mysql_select_db("pkmnazur_mmo"); $user1 = mysql_query("SELECT * FROM `users` WHERE `id`='".$_SESSION['id']."'"); $user = mysql_fetch_object($user1); ?>
+<? session_start(); $dbconf = mysql_connect("localhost","",""); $db = mysql_select_db(""); $user1 = mysql_query("SELECT * FROM `users` WHERE `id`='".$_SESSION['id']."'"); $user = mysql_fetch_object($user1); ?>
